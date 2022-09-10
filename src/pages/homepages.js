e5b61cbfbfdf2da340bb1ee5053aa2833638c165
Romadoni <romadoniofficial@gmail.com>
2022-09-11 01:54:10 +0700

    Created new Custom hooks

--- src/pages/homepages.js
+++ src/pages/homepages.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "parts/header";
-import Hero from "parts/hero";
+import Hero from "parts/Homepages/hero";
 import Clients from "parts/clients";
 import Browserroom from "parts/Homepages/browserroom";
 import Justarrived from "parts/Homepages/justarrived";
 import Sitemaps from "parts/sitemaps";
 import Footer from "parts/footer";
+import useScrollAnchor from "helpers/hooks/useScrollAnchor";
+import useModalDOM from "helpers/hooks/useModalDOM";
 
 export default function Homepage(){
+  useScrollAnchor();
+  useModalDOM()
   return(
     <>
       <Header theme="white" position="absolute"/>
